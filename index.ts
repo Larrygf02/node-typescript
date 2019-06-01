ba19945d31c398d7f840ecea4deb906440a97012
larrygf02 <raulgf_02@hotmail.com>
2019-06-01 11:22:28 -0500

    configurando server en typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Codigo de typescript');
+
+
+import Server from './server/server';
+
+const server = Server.init( 3000);
+
+server.start( () => {
+    console.log('Servidor corriendo en el puerto de 3000');
+})
